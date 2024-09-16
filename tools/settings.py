9241e3a3c5c73a3f1a31325b951783d69fee6447
umutayaz658 <umtayz658@gmail.com>
2024-09-16 14:26:40 +0300

    a

--- tools/settings.py
+++ tools/settings.py
@@ -162,7 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
